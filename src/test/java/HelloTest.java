8fa57a542600cd749426a21b7a3961d1b5ab5a13
git config --global user.name <yonashaile12@gmail.com>
2020-12-08 18:49:42 -0600

    Add @AfterAll and @BeforeAll practice

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,11 +1,40 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloTest {
 
+    //Junit annotations
+    //@BeforeAll @AfterAll @BeforeEach @AfterEach
+    @BeforeAll
+    public static void setUp(){
+        System.out.println("@BeforeAll is running");
+    }
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("@AfterAll is running");
+    }
+
+    @BeforeEach
+    public void setUpTest(){
+        System.out.println("@BeforeEach is running");
+    }
+
+    @AfterEach
+    public void tearDownTest(){
+        System.out.println("@AfterEach is running");
+    }
     @Test
     public void test(){
+        System.out.println("Test 1 is running");
         Assertions.assertEquals(4,1+3);
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("test 2 is running");
+        //assert 4 times 3 is 12
+        assertEquals(12, 3*4);
     }
 
 }
