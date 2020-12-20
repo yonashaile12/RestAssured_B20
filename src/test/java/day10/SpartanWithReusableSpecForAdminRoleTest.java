6d6225111073ab3964cd837bde987652f56ee4cf
git config --global user.name <yonashaile12@gmail.com>
2020-12-20 13:56:45 -0600

    negative scenario test

--- src/test/java/day10/SpartanWithReusableSpecForAdminRoleTest.java
+++ src/test/java/day10/SpartanWithReusableSpecForAdminRoleTest.java
@@ -23,18 +23,26 @@ public class SpartanWithReusableSpecForAdminRoleTest {
 
     static RequestSpecification givenSpec ;
     static ResponseSpecification thenSpec ;
-
-
+    static RequestSpecification postReqSpec ;
+    static Spartan randomSpartanPayload ;
     @BeforeAll
     public static void setUp(){
         baseURI = ConfigurationReader.getProperty("spartan.base_url");
-        basePath = "/api";
+        basePath = "/api" ;
         givenSpec =  given().log().all()
                 .auth().basic("admin","admin") ;
+        // log().all() will not work with expect()
+        // in order to make it work we need to use different method
+        // logDetail(LogDetail.ALL) to provide how much we want to log
         thenSpec = expect().logDetail(LogDetail.ALL)
                 .statusCode(is(200) )
                 .contentType(ContentType.JSON) ;
+        randomSpartanPayload = SpartanUtil.getRandomSpartanPOJO_Payload();
+        postReqSpec = given().spec(givenSpec)
+                .contentType(ContentType.JSON)
+                .body(randomSpartanPayload) ;
     }
+
 
     @DisplayName("GET /api/spartans/{id} Endpoint Test")
     @Test
@@ -99,6 +107,41 @@ public class SpartanWithReusableSpecForAdminRoleTest {
                 .time(lessThan(2000L))
                 .time(lessThan(2L), TimeUnit.SECONDS)
         ;
+    }
+
+    @DisplayName("Test POST /api/spartans Endpoint negative scenario")
+    @Test
+    public void testBadRequest400ResponseBody(){
+
+        Spartan badPayLoad = new Spartan("A", "A",100L);
+        String nameErrorMessage = "name should be at least 2 character and max 15 character";
+        String genderErrorMessage = "Gender should be either Male or Female";
+        String phoneErrorMessage = "Phone number should be at least 10 digit and UNIQUE!!";
+
+        given()
+                .log().all()
+                .spec(postReqSpec)
+                .body(badPayLoad).
+        when()
+                .post("/spartans").
+        then()
+                .statusCode(is(400))
+                .body("errors", hasSize(3))
+//                .body("errors[0].defaultMessage", is("Gender should be either Male or Female"))
+//                .body("errors[1].defaultMessage", is("name should be at least 2 character and max 15 character"))
+//                .body("errors[2].defaultMessage", is("Phone number should be at least 10 digit and UNIQUE!!"))
+                .body("errors.defaultMessage", containsInAnyOrder(nameErrorMessage, genderErrorMessage, phoneErrorMessage))
+                .body("message", containsString("Error count: 3"))
+        ;
+        // verify the error field has value of json array with 3 items
+        // verify default messages for those errors:
+        /*
+        "Gender should be either Male or Female"
+        "name should be at least 2 character and max 15 character"
+        "Phone number should be at least 10 digit and UNIQUE!!"
+         */
+
+
     }
 
 
